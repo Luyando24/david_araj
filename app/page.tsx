f1827fbd98f6278f078c827f9eebcd03fa70412b
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2025-11-26 21:50:43 +0200

    updated the hero image

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
                     title="DAVID ARAJ"
                     subtitle={PLAYER_INFO.position}
                     tagline={PLAYER_INFO.tagline}
-                    backgroundImage="/images/newherobg.jpg"
+                    backgroundImage="/images/newbg.jpg"
                 />
 
                 {/* Quick Stats Section */}
